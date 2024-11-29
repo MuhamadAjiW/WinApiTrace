b2da3eee379909277f79774e847c366cc7f3abcd
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-11-29 20:43:51 +0700

    feat: imported detours

--- WinApiTrace/WinApiTrace.cpp
+++ WinApiTrace/WinApiTrace.cpp
@@ -2,10 +2,20 @@
 //
 
 #include <iostream>
+#include <Windows.h>
+#include <detours/detours.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(){
+    if (DetourIsHelperProcess()) {
+        std::cout << "Detour is helper process!\n";
+        return TRUE;
+    }
+    else {
+        std::cout << "Detour is not helper process!\n";
+        return FALSE;
+    }
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
