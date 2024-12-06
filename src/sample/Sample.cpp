4322e7e768371087a4086f2d4520d0e85acb9fbf
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-12-06 11:05:12 +0700

    feat: IPC using windows named pipe sample program

--- src/sample/Sample.cpp
+++ src/sample/Sample.cpp
@@ -1,37 +1,106 @@
 // Sample.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//#include <iostream>
+//#include <windows.h>
+//#include <synchapi.h>
+//
+//int main()
+//{
+//    const WCHAR* filePath = L"testfile.txt";
+//
+//    // Call CreateFile, which internally calls NtCreateFile
+//    HANDLE hFile = CreateFile(
+//        filePath,                    // File name
+//        GENERIC_WRITE,                // Desired access
+//        0,                            // Share mode
+//        NULL,                         // Security attributes
+//        CREATE_ALWAYS,                // Creation disposition
+//        FILE_ATTRIBUTE_NORMAL,        // Flags and attributes
+//        NULL                          // Template file
+//    );
+//
+//    if (hFile == INVALID_HANDLE_VALUE) {
+//        std::cerr << "Failed to create file. Error: " << GetLastError() << std::endl;
+//    }
+//    else {
+//        std::cout << "File created successfully." << std::endl;
+//        // Optionally, write to the file
+//        const char* data = "Hello, world!";
+//        DWORD written;
+//        WriteFile(hFile, data, strlen(data), &written, NULL);
+//        std::cout << "Data written to the file." << std::endl;
+//        CloseHandle(hFile);
+//    }
+//
+//    return 0;
+//}
+
 #include <iostream>
 #include <windows.h>
-#include <synchapi.h>
+using namespace std;
 
-int main()
+int main(int argc, const char** argv)
 {
-    const WCHAR* filePath = L"testfile.txt";
+    wcout << "Creating an instance of a named pipe..." << endl;
 
-    // Call CreateFile, which internally calls NtCreateFile
-    HANDLE hFile = CreateFile(
-        filePath,                    // File name
-        GENERIC_WRITE,                // Desired access
-        0,                            // Share mode
-        NULL,                         // Security attributes
-        CREATE_ALWAYS,                // Creation disposition
-        FILE_ATTRIBUTE_NORMAL,        // Flags and attributes
-        NULL                          // Template file
+    // Create a pipe to send data
+    HANDLE pipe = CreateNamedPipe(
+        L"\\\\.\\pipe\\my_pipe", // name of the pipe
+        PIPE_ACCESS_OUTBOUND, // 1-way pipe -- send only
+        PIPE_TYPE_BYTE, // send data as a byte stream
+        1, // only allow 1 instance of this pipe
+        0, // no outbound buffer
+        0, // no inbound buffer
+        0, // use default wait time
+        NULL // use default security attributes
     );
 
-    if (hFile == INVALID_HANDLE_VALUE) {
-        std::cerr << "Failed to create file. Error: " << GetLastError() << std::endl;
-    }
-    else {
-        std::cout << "File created successfully." << std::endl;
-        // Optionally, write to the file
-        const char* data = "Hello, world!";
-        DWORD written;
-        WriteFile(hFile, data, strlen(data), &written, NULL);
-        std::cout << "Data written to the file." << std::endl;
-        CloseHandle(hFile);
+    if (pipe == NULL || pipe == INVALID_HANDLE_VALUE) {
+        wcout << "Failed to create outbound pipe instance.";
+        // look up error code here using GetLastError()
+        system("pause");
+        return 1;
     }
 
+    wcout << "Waiting for a client to connect to the pipe..." << endl;
+
+    // This call blocks until a client process connects to the pipe
+    BOOL result = ConnectNamedPipe(pipe, NULL);
+    if (!result) {
+        wcout << "Failed to make connection on named pipe." << endl;
+        // look up error code here using GetLastError()
+        CloseHandle(pipe); // close the pipe
+        system("pause");
+        return 1;
+    }
+
+    wcout << "Sending data to pipe..." << endl;
+
+    // This call blocks until a client process reads all the data
+    const wchar_t* data = L"*** Hello Pipe World ***";
+    DWORD numBytesWritten = 0;
+    result = WriteFile(
+        pipe, // handle to our outbound pipe
+        data, // data to send
+        wcslen(data) * sizeof(wchar_t), // length of data to send (bytes)
+        &numBytesWritten, // will store actual amount of data sent
+        NULL // not using overlapped IO
+    );
+
+    if (result) {
+        wcout << "Number of bytes sent: " << numBytesWritten << endl;
+    }
+    else {
+        wcout << "Failed to send data." << endl;
+        // look up error code here using GetLastError()
+    }
+
+    // Close the pipe (automatically disconnects client too)
+    CloseHandle(pipe);
+
+    wcout << "Done." << endl;
+
+    system("pause");
     return 0;
 }
