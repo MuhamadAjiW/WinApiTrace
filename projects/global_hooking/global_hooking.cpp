cbaf62cc4f110cdd5cd2a8567fd77a25c3000daf
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-12-18 14:41:01 +0700

    feat: new process monitoring

--- projects/global_hooking/global_hooking.cpp
+++ projects/global_hooking/global_hooking.cpp
@@ -5,6 +5,9 @@
 
 #include <comdef.h>
 #include <WbemIdl.h>
+#include <windows.h>
+#include <atlbase.h>
+
 
 #pragma comment(lib, "wbemuuid.lib")
 
@@ -142,6 +145,92 @@ int GetOSName() {
     return 0;
 }
 
+class CProcessCreationSink : public IWbemObjectSink {
+private:
+    long m_refCount;
+
+public:
+    CProcessCreationSink() : m_refCount(1) {}
+
+    HRESULT STDMETHODCALLTYPE QueryInterface(
+        REFIID riid,
+        _COM_Outptr_ void** ppvObject) override {
+
+        // Check for supported interfaces
+        if (riid == IID_IUnknown || riid == IID_IWbemObjectSink) {
+            *ppvObject = static_cast<IWbemObjectSink*>(this);
+            AddRef();  // Increment the reference count
+            return S_OK;
+        }
+        *ppvObject = nullptr;
+        return E_NOINTERFACE;
+    }
+
+    ULONG STDMETHODCALLTYPE AddRef() override {
+        return InterlockedIncrement(&m_refCount);  // Increment reference count atomically
+    }
+
+    ULONG STDMETHODCALLTYPE Release() override {
+        ULONG ulRefCount = InterlockedDecrement(&m_refCount);  // Decrement reference count atomically
+        if (ulRefCount == 0) {
+            delete this;  // If reference count reaches zero, delete the object
+        }
+        return ulRefCount;
+    }
+
+
+    HRESULT STDMETHODCALLTYPE Indicate(
+        long lObjectCount,
+        __RPC__in_ecount_full(lObjectCount) IWbemClassObject** ppObjArray
+    ) override {
+        // Iterate over the returned objects
+        for (long i = 0; i < lObjectCount; i++) {
+            IWbemClassObject* pObj = ppObjArray[i];
+
+            VARIANT var;
+            VariantInit(&var);
+            pObj->Get(L"TargetInstance", 0, &var, 0, 0);  // Get the TargetInstance property
+
+            IWbemClassObject* pProcess = (IWbemClassObject*) var.punkVal;
+            pProcess->Get(L"Name", 0, &var, 0, 0);  // Get the Name of the process
+
+            if (var.vt == VT_BSTR) {
+                std::wcout << L"Process created: " << var.bstrVal << std::endl;
+            }
+
+            VariantClear(&var);
+        }
+        return S_OK;
+    }
+
+    HRESULT STDMETHODCALLTYPE SetStatus(
+        long lFlags,
+        HRESULT hResult,
+        __RPC__in_opt BSTR strParam,
+        __RPC__in_opt IWbemClassObject* pObjParam
+    ) override {
+        return S_OK;
+    }
+};
+
+CComPtr<IWbemObjectSink> MonitorProcessCreation() {
+    CComPtr<IWbemObjectSink> pSink = new CProcessCreationSink();
+    //CComPtr<IWbemObjectSink> pSink = NULL;
+
+    hres = pSvc->ExecNotificationQueryAsync(
+        _bstr_t(L"WQL"),
+        _bstr_t(L"SELECT * FROM __InstanceCreationEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_Process'"),
+        0,
+        NULL,
+        pSink
+    );
+    if (FAILED(hres)) {
+        std::cout << "Query for process creation failed. Error code = 0x" << std::hex << hres << std::endl;
+    }
+
+    return pSink;
+}
+
 ///////////////////////////////////////////////////////////////// End of File.
 //  Main
 
@@ -153,7 +242,10 @@ int main(){
         return 1;
     }
 
-    GetOSName();
+    CComPtr<IWbemObjectSink> pSink = MonitorProcessCreation();
+
+    while (true);
+
     UnInitializeWMI();
     std::cout << "Program Finished\n";
 
